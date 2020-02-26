16e1436e18814d67b070b1062061ce89d8197a32
unknown <keesmark@gmail.com>
2020-02-26 14:30:33 +0900

    'task'

--- server/app/Folder.php
+++ server/app/Folder.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Task;
 
 class Folder extends Model
 {
-    //
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
